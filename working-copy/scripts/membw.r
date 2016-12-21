514c04c1fe4ef23032bddce6ebda4eda61ad6cdd
Aniraj Kesavan <aniraj@cs.utah.edu>
2016-12-20 20:04:41 -0700

    error logs

--- working-copy/scripts/membw.r
+++ working-copy/scripts/membw.r
@@ -1,11 +1,32 @@
 source('common.R')
 
-makeZeroCopyTputFigure <- function () {
-  d <- tputload()
-  head(d)
+membwfilenames <- Sys.glob("/Users/aniraj/development/thesis/working-copy/data/singledatapoints/cx3/pcieddioimc/*.csv")
+tputfilenames <- Sys.glob("/Users/aniraj/development/thesis/working-copy/data/singledatapoints/cx3/pcieddioimc/*out.log")
+
+mergealltputfiles <- function(){
+  fileNumbers <- seq(tputfilenames)
+  mergedfilename <- paste(file_path_sans_ext(tputfilenames[1]),"-merged.out",sep = "")
+  print(mergedfilename)
+  for (filenumber in fileNumbers){
+    if (filenumber == 1){
+    d <- read.table(tputfilenames[filenumber],header=T)
+    print(head(d,1))
+    }
+    else{
+      d <- read.table(tputfilenames[filenumber],header=F)
+      print(head(d,1))
+    }
+    write.table(d,mergedfilename,append=T,row.names = FALSE,
+                col.names = FALSE,quote=FALSE)
+  }
+}
+
+makeZeroCopyTputFigure <- function (filename='') {
+  d <- tputload(filename)
   d <- d[d$chunkSize %in% c(128,1024),]
   p <- myplot(d) +
-    coord_cartesian(ylim=c(0, 8000), xlim=c(128, 16 * 1024))
+    coord_cartesian(ylim=c(0, 8000), xlim=c(128, 16 * 1024))+
+  ggtitle(substr(file_path_sans_ext(basename(filename)),14,45))
   p
   #ggsave(plot=p, filename='~/development/thesis/working-copy/figures/cx3_noperf/20161109180229-15clients/fig-zero-copy-tput.pdf',
   #       width=5, height=2, units='in')
@@ -44,7 +65,7 @@ aggregateClients <- function (d) {
         #aggMBs=sum(transmissions * (as.double(deltasPerMessage * deltaSize) +
         #                            as.double(chunksPerMessage * chunkSize)))
         #           / 2^20 / max(seconds),
-        aggMBs=sum(transmittedBytes) / 2^20 / max(seconds),
+        aggMBs=sum(as.numeric(transmittedBytes)) / 2^20 / max(seconds),
         clients=length(server))
 }
 
@@ -52,30 +73,56 @@ aggregateClients <- function (d) {
 tputload <- function (filename='/Users/aniraj/development/thesis/working-copy/data/singledatapoints/cx3/pcieddioimc/201612172047-15-clients-128B-32chunks-r320-out.log') {
   d <- read.table(filename
                   , header=T)
+  d <- d[complete.cases(d),]
   d$bytesPerMessage <- d$chunkSize * d$chunksPerMessage
-  #d$chunkSize <- factor(d$chunkSize)
   d$copied <- factor(d$copied)
   d
 }
 
 membwload <- function (filename='/Users/aniraj/development/thesis/working-copy/data/singledatapoints/cx3/pcieddioimc/201612172047-15-clients-128B-32chunks-r320-membw.csv') {
   d <- read.csv(filename,header=T)
-  d
+  d <- d[complete.cases(d),]
 }
-makeMemoryBWFigure <- function(){
-a <- membwload()
+makeMemoryBWFigure <- function(filename='',extratitle=''){
+if(is.null(filename)){
+  return()
+}
+print(filename)
+a <- membwload(filename)
+print(head(a,1))
 a["membw"]<-(a$iMC0.MEM_BW_TOTAL+a$iMC1.MEM_BW_TOTAL+a$iMC2.MEM_BW_TOTAL)*10
 a["pciebw"]<-a$CBO.LLC_PCIE_MEM_TOTAL_BYTES*10
 a["ddiobw"]<-a$CBO.LLC_DDIO_MEM_TOTAL_BYTES*10
+titlestring<-paste(substr(file_path_sans_ext(basename(filename)),14,44),extratitle, sep="\n")
 
 p<- ggplot(a,aes(x=timestamp,y=membw,color="membw"))+
      geom_point()+
      geom_line()+
      geom_line(aes(y=pciebw,color="pciebw"))+
      geom_line(aes(y=ddiobw,color="ddiobw"))+
-     geom_vline(xintercept = 47,colour="black", linetype = "longdash", show.legend = T)+
-     geom_vline(xintercept = 87,colour="grey", linetype = "longdash", show.legend = T)+
+     geom_line(aes(y=ddiobw,color="ddiobw"))+
+     geom_line(aes(y=ddiobw,color="ddiobw"))+
+     geom_line(aes(y=ddiobw,color="ddiobw"))+
      coord_cartesian(ylim=c(0, 8000))+
-     ggtitle("Various metrics at Zero Copy,Unloaded and Copy Out\niMC.MEM_BW_TOTAL=membw\nCBO.LLC_DDIO_MEM_TOTAL_BYTES=ddiobw\nCBO.LLC_PCIE_MEM_TOTAL_BYTES=pciebw")
+     #ggtitle("0 copy - no load - copyout\niMC.MEM_BW_TOTAL=membw\nCBO.LLC_DDIO_MEM_TOTAL_BYTES=ddiobw\nCBO.LLC_PCIE_MEM_TOTAL_BYTES=pciebw")+
+     ggtitle(titlestring)
 p
+}
+makeallmembwfigures<-function(){
+for (membwfile in membwfilenames){
+  tryCatch({
+  print(paste("doing - ", substr(file_path_sans_ext(basename(membwfile)),14,45)))
+  p<-makeMemoryBWFigure(membwfile)
+  print(paste("done - ", substr(file_path_sans_ext(basename(membwfile)),14,45)))
+  outputfilename<-paste("/Users/aniraj/development/thesis/working-copy/figures/singledatapoints/membwandtput/",basename(file_path_sans_ext(membwfile)),".pdf",sep="")
+  ggsave(outputfilename,width=4,height=4,units='in')
+  },error=function(e){cat("ERROR :",conditionMessage(e), "\n")})
+}
+}
+makealltputfigures<-function(){
+  for (tputfile in tputfilenames){
+    p<-makeZeroCopyTputFigure(tputfile)
+    outputfilename<-paste("/Users/aniraj/development/thesis/working-copy/figures/singledatapoints/membwandtput/",basename(file_path_sans_ext(tputfile)),"-tput.pdf",sep="")
+    ggsave(outputfilename,width=4,height=4,units='in')
+  }
 }
